ce46b572b14c0dde3c97fdbad8174b36f9e9e7c8
Emmanuel Effa <manueleffa@gmail.com>
2026-02-22 16:36:31 +0100

    built the html template for the app

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,46 @@ function App() {
 
   return (
     <>
-      
+      <div>
+        <aside>
+          <div>
+            <h1>MovieBase</h1>
+          </div>
+
+          <ul>
+            <li>Home</li>
+            <li>TV Show</li>
+            <li>Action</li>
+            <li>Comedy</li>
+            <li>Sethings</li>
+          </ul>
+        </aside>
+
+        <main>
+          <div>
+            <h2>Home</h2>
+            <div>
+              <input type="search" id='search'placeholder='Search Movies By Title....'/>
+              <button>Search</button>
+            </div>
+          </div>
+
+          <div>
+            <h2>Popular Movies</h2>
+            <div>
+              <div>
+                <div>
+                  <img src="" alt="" />
+                </div>
+                <div>
+                  <h3>The Extraction</h3>
+                  <i></i>
+                </div>
+              </div>
+            </div>
+          </div>
+        </main>
+      </div>
     </>
   )
 }
