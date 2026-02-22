f7b2ab9d4ff25b7d5c5393c0c18fcedbcb214a35
Emmanuel Effa <manueleffa@gmail.com>
2026-02-22 17:02:53 +0100

    added some styling to the HTML template to define the sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,33 @@
 import { useState } from 'react'
 import './App.css'
+import './index.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <aside>
-          <div>
+      <div className='bg-blue-950 text-white h-screen grid grid-cols-4'>
+        <aside className='col-span-1'>
+          <div className='mb-5'>
             <h1>MovieBase</h1>
           </div>
 
-          <ul>
+          <div className='flex flex-col justify-between'>
+            <ul>
             <li>Home</li>
             <li>TV Show</li>
             <li>Action</li>
             <li>Comedy</li>
+          </ul>
+          <ul>
             <li>Sethings</li>
           </ul>
+          </div>
         </aside>
 
-        <main>
-          <div>
+        <main className='col-span-3'>
+          <div className='flex justify-between mb-5'>
             <h2>Home</h2>
             <div>
               <input type="search" id='search'placeholder='Search Movies By Title....'/>
