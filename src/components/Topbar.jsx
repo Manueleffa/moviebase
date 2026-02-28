a967e7b5d954c6725e278f3c1e528f4100db308e
Emmanuel Effa <manueleffa@gmail.com>
2026-02-28 20:00:02 +0100

    updated the top bar to track what user search and navigate to the searchpage

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -1,13 +1,28 @@
 import React from 'react'
+import { useNavigate, useNavigate } from 'react-router-dom'
 
 const Topbar = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const { useNavigate } = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const trimmed = searchTerm.trim();
+    if (!trimmed) return;
+
+    navigate(`/search?q=${trimmed}`)
+  };
+
   return (
     <div className="flex items-center justify-between gap-6 mb-5 border border-[#3a414e] rounded-md px-3 py-4">
         <h2 className="text-lg font-semibold">Home</h2>
-        <form action="" className="flex gap-x-1 items-center">
+        <form onSubmit={handleSubmit} className="flex gap-x-1 items-center">
           <input
             type="search"
-            placeholder="Search movies ..."
+            placeholder="Search movies by Title ..."
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
             className="bg-black placeholder:text-xs px-4 border border-[#3a414e] rounded-tl-3xl rounded-bl-3xl w-2xl h-10"
           />
           <button
