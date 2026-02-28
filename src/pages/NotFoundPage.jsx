f8ec9eb0886b30512b94383887e8999b9fb0e41e
Emmanuel Effa <manueleffa@gmail.com>
2026-02-28 22:46:39 +0100

    build and styled the notFoundPage

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const NotFoundPage = () => {
   return (
-    <div>404 error</div>
+    <div className='text-center flex flex-col items-center justify-center h-[70vh]'>
+      <h1 className='text-6xl mb-6 font-bold text-yellow-700'>404 <br /> Error</h1>
+      <p className='text-2xl'>Page Not Found</p>
+    </div>
   )
 }
 
