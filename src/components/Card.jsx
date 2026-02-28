2339279e435c5ffb809f2be8d9b1f9a53ca4ac32
Emmanuel Effa <manueleffa@gmail.com>
2026-02-28 12:09:56 +0100

    updated the card component to display real data

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,17 +2,19 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import placeHolder from "../assets/placeHolder.png";
 
-const Card = () => {
+const Card = ({ movie }) => {
+  const { Title, Year, Poster, imdbID } = movie;
+
   return (
     <div className=" bg-yellow-700 border border-yellow-700 rounded overflow-x-hidden">
-      <NavLink to="/moviedetails">
+      <NavLink to={`/moviedetails/${imdbID}`}>
         <div className="w-full pb-4">
-          <img src={placeHolder} alt="" className="w-full" />
+          <img src={Poster !== "N/A" ? Poster : placeHolder} alt={Title} className="w-full" />
         </div>
         <div>
-          <h3 className="text-md font-bold px-2">The Extraction</h3>
+          <h3 className="text-md font-bold px-2">{Title}</h3>
           <p className="text-[14px] px-2 font-semibold mb-4">
-            Realease Date: xxxx/xx/xx
+            Realease Date: {Year}
           </p>
         </div>
       </NavLink>
