f86c0bb6015292dc1fb1a3071f6a27bbe35d1665
Emmanuel Effa <manueleffa@gmail.com>
2026-02-28 12:55:01 +0100

    update cards to fetch and pass data

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,13 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Card from "./Card";
+import { searchMovies } from "../utilities/omdb";
 
-const Cards = () => {
+const Cards = ({ query, type = "movie" }) => {
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(()=> {
+    const fetchMovies = async () => {
+      setLoading(true);
+      setError(null);
+
+      const data = await searchMovies(query, type);
+
+      if (data.Response === "True") {
+        setMovies(data.Search);
+      } else {
+        setError(data.Error);
+      }
+
+      setLoading(false);
+    };
+
+    fetchMovies();
+  }, [query]);
+
+  if (loading) return <p className="text-center">Loading movies....</p>;
+  if (error) return <p className="text-center text-red-500">{error}</p>
+
+
+
+
   return (
     <div className="grid grid-cols-4 gap-4 mb-6">
-      <Card />
-      <Card />
-      <Card />
-      <Card />
+      {movies.map((movie) => (
+        <Card key={movie.imdbID} movie={movie} />
+      ))}
     </div>
   );
 };
