d9982e11082c4abbbeef9864122effe55a88f058
Emmanuel Effa <manueleffa@gmail.com>
2026-02-28 23:04:12 +0100

    updated the topbar to show page name on the title

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -1,9 +1,22 @@
 import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 
 const Topbar = () => {
   const [searchTerm, setSearchTerm] = useState("");
   const navigate = useNavigate();
+  const location = useLocation();
+
+  const pageNames = {
+    "/": "Home",
+    "/action": "Action",
+    "/comedy": "Comedy",
+    "/tvshows": "Series",
+    "/search": "Search",
+    "/moviedetails": "Details",
+    "*": "Error",
+  };
+
+  const currentPage = pageNames[location.pathname] || "Movies";
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -17,7 +30,7 @@ const Topbar = () => {
 
   return (
     <div className="flex items-center justify-between gap-6 mb-5 border border-[#3a414e] rounded-md px-3 py-4">
-      <h2 className="text-lg font-semibold">Home</h2>
+      <h2 className="text-lg font-semibold">{currentPage}</h2>
       <form onSubmit={handleSubmit} className="flex gap-x-1 items-center">
         <input
           type="search"
