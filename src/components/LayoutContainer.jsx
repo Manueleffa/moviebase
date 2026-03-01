87f43765b632636781e6e1dd11698b0f55fb813a
Emmanuel Effa <manueleffa@gmail.com>
2026-03-01 10:53:46 +0100

    updated the layoutcontainer to manage sidebar state

--- src/components/LayoutContainer.jsx
+++ src/components/LayoutContainer.jsx
@@ -1,14 +1,25 @@
-import React from 'react'
-import Sidebar from './Sidebar'
-import MainArea from './MainArea'
+import React from "react";
+import Sidebar from "./Sidebar";
+import MainArea from "./MainArea";
 
 const LayoutContainer = () => {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => setSidebarOpen(!sidebarOpen);
+  const closeSidebar = () => setSidebarOpen(false);
+
   return (
     <div className="bg-[#020d18] text-white h-screen grid grid-cols-4">
-        <Sidebar />
-        <MainArea />
+      {sidebarOpen && (
+        <div
+          className="fixed inset-0 bg-black bg-opacity-60 z-10 md:hidden"
+          onClick={closeSidebar}
+        />
+      )}
+      <Sidebar sidebarOpen={sidebarOpen} closeSidebar={closeSidebar} />
+      <MainArea toggleSidebar={toggleSidebar} />
     </div>
-  )
-}
+  );
+};
 
-export default LayoutContainer
+export default LayoutContainer;
